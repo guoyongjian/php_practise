5c096fe395faefe3890fddd1b012e7d0c5bb4ef1
guo <1147657469@qq.com>
2018-05-01 23:20:20 -0400

    update

--- getIpAddress.php
+++ getIpAddress.php
@@ -22,18 +22,18 @@
         return $realip;
     }
     
-    echo $ip = getIP();
+ $ip = getIP();
 
 //淘宝接口根据ip查询所在区域信息
-echo '淘宝' . PHP_EOL;
+//echo '淘宝' . PHP_EOL;
 $res1 = file_get_contents("http://ip.taobao.com/service/getIpInfo.php?ip=$ip");
 $res1 = json_decode($res1,true);
-if(!empty($res1['code']){
+if(!empty($res1['code'])){
     exit('get data error');
 }
 
-echo '您的ip： ' . $res1['ip'] . PHP_EOL;
-echo '您的国家： ' . $res1['country'] . PHP_EOL;
-echo '您的省份： ' . $res1['region'] . PHP_EOL;
-echo '您的城市： ' . $res1['city'] . PHP_EOL;
-echo '您的运营商： ' . $res1['isp'] . PHP_EOL;
+echo '您的ip： ' . $res1['data']['ip'] . PHP_EOL;
+echo '您的国家： ' . $res1['data']['country'] . PHP_EOL;
+echo '您的省份： ' . $res1['data']['region'] . PHP_EOL;
+echo '您的城市： ' . $res1['data']['city'] . PHP_EOL;
+echo '您的运营商： ' . $res1['data']['isp'] . PHP_EOL;
