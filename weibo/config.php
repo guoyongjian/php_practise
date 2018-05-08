0515d0907c05cf33992c6e4f3b4b564bf41a158c
guoyongjian <guoyongjian@hoge.cn>
2018-05-08 10:18:33 +0800

    add weibo

---
+++ weibo/config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '1067521494' );
+define( "WB_SKEY" , '6adce8bd7bc51481949fa3bd300c80e1' );
+define( "WB_CALLBACK_URL" , 'http://weibo.guoyognjian.top/callback.php' );
